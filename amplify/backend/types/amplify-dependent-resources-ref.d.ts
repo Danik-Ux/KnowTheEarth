388c13ee3cea578e27e90e19778a16d92dcb8468
Danik-Ux <aimdannewz@gmail.com>
2024-08-22 12:54:28 +0300

    Fixing minor bug with map

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,11 +1,4 @@
 export type AmplifyDependentResourcesAttributes = {
-  "api": {
-    "knowtheearth": {
-      "GraphQLAPIEndpointOutput": "string",
-      "GraphQLAPIIdOutput": "string",
-      "GraphQLAPIKeyOutput": "string"
-    }
-  },
   "auth": {
     "Knowtheearth": {
       "AppClientID": "string",
